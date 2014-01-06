3dea71e2c9d2ead5900e3cc8b1562213607b61b4
wei_jc <weijiancai@126.com>
2014-01-06 22:02:32 +0800

    muForm add controller method

--- src/angularjs/js/directives/mu-form.js
+++ src/angularjs/js/directives/mu-form.js
@@ -1,5 +1,8 @@
 metauiDirectives.directive('muForm', function () {
     return {
-        templateUrl: 'js/templates/formTpl.html'
+        templateUrl: 'js/templates/formTpl.html',
+        controller: function($scope, $element, $attrs, $transclude) {
+
+        }
     };
 });
